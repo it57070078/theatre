fc70343d2a4f2349423dd535a311e836a2df0986
marktrs <s7070041@kmitl.ac.th>
2014-12-08 22:54:15 +0700

    edit

--- theatre_admin.py
+++ theatre_admin.py
@@ -31,6 +31,7 @@ class Theatre:
         self.button_inframe()
         self.show_detail_list()
         self.show_overview()
+        self.round_manage()
 
         template()
         mainloop()
@@ -39,23 +40,22 @@ class Theatre:
         '''show last update time'''
         Label(self.root, bg='white', text=strftime('Last update : %A %d %B %Y')).place(x = 310, y = 5)
 
+    f_data = open('data.txt', 'r')
+    data = [map(lambda x: x,i.split()) for i in f_data]
 
     def button_inframe(self):
         ''' show all button'''
         Button(self.root, text = 'Update')\
-        .place(x = 620, y = 460, width = 50, height = 20) #Update
+        .place(x = 550, y = 460, width = 50, height = 20) #Update
         Button(self.root, text = 'Close', command = quit)\
-        .place(x = 740, y = 460, width = 50, height = 20)  #Close program
+        .place(x = 690, y = 460, width = 50, height = 20)  #Close program
         Button(self.root, text = 'Detail').\
-        place(x = 680, y = 460, width = 50, height = 20) #Description of frame composition
+        place(x = 620, y = 460, width = 50, height = 20) #Description of frame composition
 
     def show_detail_list(self):
         """ show list of data and detail (included date,movie,time,name,amout, price ,seat)"""
-        f_data = open('data.txt', 'r')
-        data = [map(lambda x: x,i.split()) for i in f_data]
-
-        y, num = 90, len(data)
-        for i in data[-10:]:
+        y, num = 90, len(self.data)
+        for i in self.data[-10:]:
             line = str(num)+'.'+(' '*10)
             line += i[0][5:15].center(12) + i[1].rjust(30) + i[2].rjust(30)
             line += (' '*20) + i[3].ljust(40)
@@ -67,11 +67,8 @@ class Theatre:
 
     def show_overview(self):
 
-        f_data = open('data.txt', 'r')
-        data = [map(lambda x: x,i.split()) for i in f_data]
-
-        total_seat = ' Audience : ' + str(sum([int(x[4]) for x in data]))
-        total_income = 'Income : ' + str(sum([int(x[5]) for x in data]))
+        total_seat = ' Audience : ' + str(sum([int(x[4]) for x in self.data]))
+        total_income = 'Income : ' + str(sum([int(x[5]) for x in self.data]))
 
         Label(self.root, text='Statistic').place(x = 50, y = 250)
         Label(self.root, text='Max audience  Cinema : -     Movie : -').place(x = 30, y = 280)
@@ -82,13 +79,28 @@ class Theatre:
         Label(self.root, text='Total sold ticket in week : ').place(x = 30, y = 360)
         Label(self.root, text='Total sold ticket in month : ').place(x = 30, y = 380)
 
-        Label(self.root, text='Present day stat').place(x = 50, y = 420)
-        Label(self.root, text=total_seat).place(x = 30, y = 440)
-        Label(self.root, text=total_income).place(x = 150, y = 440)
+        Label(self.root, text='Present day stat').place(x = 50, y = 410)
+        Label(self.root, text=total_seat).place(x = 30, y = 430)
+        Label(self.root, text=total_income).place(x = 150, y = 430)
 
     def round_manage(self):
         '''Enable/Disable time for each cinema'''
-        
+        y,time = 280, ['10.45', '11.30', '12.15', '13.00', '13.45', '14.30', '15.15']
+        Label(self.root, text='Cinema').place(x = 580, y = 235)
+        Label(self.root, text='Time').place(x = 450, y = 255)
+
+        for j in xrange(6):
+            Label(self.root, text=str(j+1)).place(x = (500+(j*45)), y = 260)
+
+        for i in xrange(7):
+            Label(self.root, text=time[i]).place(x = 450, y = y)
+            x = 450
+            for i in xrange(6):
+                x += 45
+                Checkbutton(self.root).place(x = x, y = y)
+            y += 25
+
+
 
 Theatre()
 
