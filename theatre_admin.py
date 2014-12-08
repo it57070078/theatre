0994d73a86b5f6a3813f5bc413fd3b3192867660
marktrs <s7070041@kmitl.ac.th>
2014-12-08 18:07:56 +0700

    Re-arrange method in class

--- theatre_admin.py
+++ theatre_admin.py
@@ -25,23 +25,19 @@ class Theatre:
             header = Label(image = header_pic)
             header.image = header_pic # keep a reference!
             header.place(x = 0, y = 0)
+            self.update_time()
 
-        f_data = open('data.txt', 'r')
-        data = [map(lambda x: x,i.split()) for i in f_data]
 
         self.button_inframe()
-        self.update_time()
         self.show_detail_list()
-        #self.show_overview(data)
+        self.show_overview()
 
         template()
         mainloop()
-        self.root.destroy()
 
     def update_time(self):
         '''show last update time'''
         Label(self.root, bg='white', text=strftime('Last update : %A %d %B %Y')).place(x = 310, y = 5)
-        day, month, year = IntVar(), IntVar(), IntVar()
 
 
     def button_inframe(self):
@@ -53,8 +49,11 @@ class Theatre:
         Button(self.root, text = 'Detail').\
         place(x = 680, y = 460, width = 50, height = 20) #Description of frame composition
 
-    def show_detail_list(data):
+    def show_detail_list(self):
         """ show list of data and detail (included date,movie,time,name,amout, price ,seat)"""
+        f_data = open('data.txt', 'r')
+        data = [map(lambda x: x,i.split()) for i in f_data]
+
         y, num = 90, len(data)
         for i in data[-10:]:
             line = str(num)+'.'+(' '*10)
@@ -66,8 +65,11 @@ class Theatre:
             y += 20
             num -= 1
 
-    '''
-    def show_overview(data, self):
+    def show_overview(self):
+
+        f_data = open('data.txt', 'r')
+        data = [map(lambda x: x,i.split()) for i in f_data]
+
         total_seat = ' Audience : ' + str(sum([int(x[4]) for x in data]))
         total_income = 'Income : ' + str(sum([int(x[5]) for x in data]))
 
@@ -83,7 +85,10 @@ class Theatre:
         Label(self.root, text='Present day stat').place(x = 50, y = 420)
         Label(self.root, text=total_seat).place(x = 30, y = 440)
         Label(self.root, text=total_income).place(x = 150, y = 440)
-    '''
+
+    def round_manage(self):
+        '''Enable/Disable time for each cinema'''
+        
 
 Theatre()
 
