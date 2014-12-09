fcdc2a889885c913ac683efd81d2215bcad398c2
it57070078 <s7070078@kmitl.ac.th>
2014-12-09 19:28:37 +0700

    update

--- TK_project.py
+++ TK_project.py
@@ -5,9 +5,19 @@ import tkMessageBox
 import tkFont
 import ttk
 def naxt():
+    mGui.destroy()
     nGui = Tk()
     nGui.geometry('800x600+500+200')
     nGui.title('Theatre Maneger')
+    nlabel = Label(nGui,text='Buy Ticket',bg = 'gray', font = tkFont.Font(size = 30, weight=tkFont.BOLD)).place(x=50,y=0)
+    nlabel2 = Label(nGui,text='Current Date :'+strftime('%d %B %Y'),bg = 'gray',  font = tkFont.Font(size = 10, weight=tkFont.NORMAL)).place(x= 450 ,y=0)
+
+    
+
+
+
+
+    nGui.config(background = 'gray')
 def out():
     mExit = tkMessageBox.askyesno(title="Quit",message="Are You Sure")
     if mExit > 0:
@@ -25,21 +35,67 @@ def creator():
 def thanks():
     tkMessageBox.showinfo(title='Thanks',message='    THANKS  \n Chotipat Pornavalai ')
     return
+## Poster office
+def pic_1():
+    header_pic = PhotoImage(file = "001.gif")
+    header = Label(image = header_pic)
+    header.image = header_pic 
+    header.place(x = 8, y = 90)
+def pic_2():
+    header_pic = PhotoImage(file = "002.gif")
+    header = Label(image = header_pic)
+    header.image = header_pic 
+    header.place(x = 8, y = 200)
+def pic_3():
+    header_pic = PhotoImage(file = "003.gif")
+    header = Label(image = header_pic)
+    header.image = header_pic 
+    header.place(x = 8, y = 310)
+def pic_4():
+    header_pic = PhotoImage(file = "004.gif")
+    header = Label(image = header_pic)
+    header.image = header_pic 
+    header.place(x = 8, y = 420)
+def get_movie(name, time):
+    movielabel = Label(text= name,bg = 'gray',  font = tkFont.Font(size = 15, weight=tkFont.NORMAL)).place(x=225 ,y=550)
+    roundlabel = Label(text= time,bg = 'gray',  font = tkFont.Font(size = 15, weight=tkFont.NORMAL)).place(x=450 ,y=550)
 
-
+#make time select
+def time_select(mGui):
+    time_data = open('time.txt', 'r')
+    time = [map(lambda x: x,i.split()) for i in time_data]
+    y = 100
+    for i in time:
+        x = 440
+        count = 0
+        for j in i[1:]:
+            if count == 5:
+                y += 35
+                x = 440
+            Button(mGui, text=str(j),command = get_movie(i[0],j)).place(x = x, y = y)
+            print i[0] , j
+            x += 50
+            count += 1
+        if count > 5:
+            y -= 35
+        y += 110
+        
 mGui = Tk() 
 mGui.geometry('800x600+550+200')
 mGui.title('Theatre Manager')
 #make label
 mlabel = Label(text='BOX OFFICE',bg = 'gray', font = tkFont.Font(size = 30, weight=tkFont.BOLD)).place(x=50 ,y=0)
-mlabel2 = Label(text='Current Date :'+strftime('%d %B %Y'),bg = 'gray',  font = tkFont.Font(size = 10, weight=tkFont.NORMAL)).place(x= 450 ,y=0)
-mlabel3 = Label(text='Movies',bg = 'gray',  font = tkFont.Font(size = 10, weight=tkFont.BOLD)).place(x=125 ,y=60)
+mlabel2 = Label(text='Current Date :'+strftime('%d %B %Y'),bg = 'gray',  font = tkFont.Font(size = 10, weight=tkFont.NORMAL)).place(x= 300 ,y=5)
+mlabel3 = Label(text='Movies',bg = 'gray',  font = tkFont.Font(size = 10, weight=tkFont.BOLD)).place(x=175 ,y=60)
 mlabel4 = Label(text='Round Time',bg = 'gray',  font = tkFont.Font(size = 10, weight=tkFont.BOLD)).place(x=500 ,y=60)
+mlabel5 = Label(text='Current Movie :',bg = 'gray',  font = tkFont.Font(size = 15, weight=tkFont.NORMAL)).place(x=75 ,y=550)
+mlabel6 = Label(text='Round :',bg = 'gray',  font = tkFont.Font(size = 15, weight=tkFont.NORMAL)).place(x=365 ,y=550)
+ 
 
 
 #make buttom
-mbutton = Button(text = 'Summit',command = naxt).place(x=500,y=500)
-mbutton = Button(text = 'Quit',command = out).place(x=600,y=500)
+mbutton = Button(text = 'Summit',command = naxt).place(x=550,y=550)
+mbutton2 = Button(text = 'Quit',command = out).place(x=650,y=550)
 
 #make About menu
 menubar = Menu(mGui)
@@ -54,13 +110,22 @@ helpmenu.add_command(label='Help Docs', command = helpdocs)
 helpmenu.add_command(label='About' , command = about)
 menubar.add_cascade(label="Help",menu=helpmenu)
 
+#make Canvas movie
+canvas_1 = Canvas(mGui,height = 100,width= 225,bg = '#2E8B57').place(x=160,y=90)
+canvas_2 = Canvas(mGui,height = 100,width= 225,bg = '#2E8B57').place(x=160,y=200)
+canvas_3 = Canvas(mGui,height = 100,width= 225,bg = '#2E8B57').place(x=160,y=310)
+canvas_4 = Canvas(mGui,height = 100,width= 225,bg = '#2E8B57').place(x=160,y=420)
+
+#make Canvas Round Time
+canvas_5 = Canvas(mGui,height = 100,width= 270,bg = '#2E8B57').place(x=430,y=90)
+canvas_6 = Canvas(mGui,height = 100,width= 270,bg = '#2E8B57').place(x=430,y=200)
+canvas_7 = Canvas(mGui,height = 100,width= 270,bg = '#2E8B57').place(x=430,y=310)
+canvas_8 = Canvas(mGui,height = 100,width= 270,bg = '#2E8B57').place(x=430,y=420)
 
 
 
-
-
-
-
+pic_1(),pic_2(),pic_3(),pic_4()
+time_select(mGui)
 mGui.config(menu = menubar, background= 'gray')
 
 
