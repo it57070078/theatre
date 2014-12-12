55120ce7fdb1df17d7c3ea18ab2e4a9ef3d2a66e
it57070078 <s7070078@kmitl.ac.th>
2014-12-12 19:48:04 +0700

    ได้ละ

--- TK_project.py
+++ TK_project.py
@@ -104,9 +104,10 @@ def pic_4():
 #make time select
 time_button = []
 time_val = []
-def selected(i, j):
+def selected(val):
     print 'opening selected',
-    data = time_val[i][j].get()
+    data = val
+    print data
     Label(text=data[:4],fg = 'white', bg ='#464646').place(x=225 ,y=550)
     Label(text=data[-5:],fg = 'white', bg ='#464646').place(x=450 ,y=550)
 
@@ -121,25 +122,15 @@ def time_select(mGui):
         time_button.append([])
         for j in xrange(1, len(time[i])):
             print 'time[i][j] == ',time[i][j],'i ='+str(i),'j='+str(j)
-            val = StringVar()
-            val.set(time[i][0]+' '+str(time[i][j]))
-            time_val[i].append(val)
-            temp = Button(text=str(time[i][j]), textvariable = val.get())
-            print 'time_val >>>',time_val[i][j-1].get()
-            temp.config(command = lambda : selected(i, j))
+            #val = StringVar()
+            #val.set(time[i][0]+' '+str(time[i][j]))
+            val = time[i][0]+' '+str(time[i][j])
+            time_val[i].append(time[i][0]+' '+str(time[i][j]))
+            #temp = Button(text=str(time[i][j]), textvariable = val.get())
+            temp = Button(text=str(time[i][j]))
+            #print 'time_val >>>',time_val[i][j-1].get()
+            temp.config(command = lambda value = val: selected(value))
             time_button[i].append(temp)
-            print '--------------'
-
-    for i in time_val:
-        for j in i:
-            print j.get()+'    ',
-        print ''
-    print ''
-    for i in time_button:
-        for j in i:
-            print  j['textvariable'],'   ',
-        print ''
-
     for i in time_button:
         x = 440
         count = 0
